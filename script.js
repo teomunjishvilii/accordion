e98b12036a21cc0e1d9a590b483857fab26a1ee5
Teo Munjishvili <teona.munjishvili.1@iliauni.edu.ge>
2024-08-05 16:35:13 +0400

    fix js code

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-const accordion_div = document.querySelectorAll(".accordion_header");
-const accordion_body = document.querySelector(".accordion_body");
-const accordion_header = document.querySelectorAll("accordion_header");
-const arrowIcon = document.querySelector(".arrow i");
+const accordion_div = document.querySelectorAll(".accordion_item");
 
 accordion_div.forEach(function (item) {
   item.addEventListener("click", function () {
-    accordion_body.classList.toggle("active");
-
-    //change the bg color of the accordion header when clicked
-    this.classList.toggle("clicked");
+    console.log(this);
+    this.classList.toggle("active");
 
     // change the arrow when clicked + scroll down smoothly
-    if (accordion_body.classList.contains("active")) {
+    const arrowIcon = this.querySelector(".arrow i");
+
+    if (this.classList.contains("active")) {
       arrowIcon.classList.remove("fa-chevron-down");
       arrowIcon.classList.add("fa-chevron-up");
-      accordion_body.style.height = accordion_body.scrollHeight + "px";
     } else {
       arrowIcon.classList.add("fa-chevron-down");
       arrowIcon.classList.remove("fa-chevron-up");
-      accordion_body.style.height = 0;
     }
   });
 });
